1115dee0d2eff54fdd9ed8fd47e5ef7fbf4dafbe
shchau <shchau1@ualberta.ca>
2017-10-22 21:17:11 -0600

    Barebones xml

---
+++ app/src/main/java/ssmad/habitizer/HabitTabActivity.java
@@ -0,0 +1,17 @@
+package ssmad.habitizer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cryst on 10/22/2017.
+ */
+
+public class HabitTabActivity extends AppCompatActivity{
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.habit_tab);
+        }
+
+}
