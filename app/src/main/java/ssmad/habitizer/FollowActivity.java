5cf65f55cb3b9408ae15ab38b0310b0ed6b04892
dwai <dwai@ualberta.ca>
2017-10-23 16:02:33 -0600

    FollowActivity and FollowActivityTest added

---
+++ app/src/main/java/ssmad/habitizer/FollowActivity.java
@@ -0,0 +1,20 @@
+package ssmad.habitizer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.robotium.solo.Solo;
+
+/**
+ * Created by dwai on 2017-10-23.
+ */
+
+public class FollowActivity extends AppCompatActivity {
+
+    private Solo solo;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.social_tab);
+    }
+}
