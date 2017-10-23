a1b771ae0e6ab243ab7d4babcba8aaad4774747b
shchau <shchau1@ualberta.ca>
2017-10-23 14:20:39 -0600

    xml for Add Habit/ Add Habit Event and minor changes

--- app/src/test/java/ssmad/habitizer/HabitTabActivityTest.java
+++ app/src/test/java/ssmad/habitizer/HabitTabActivityTest.java
@@ -33,13 +33,11 @@ public class HabitTabActivityTest extends ActivityInstrumentationTestCase2<Habit
         solo.assertCurrentActivity("Wrong Activity!", AddHabitActivity.class);
         solo.enterText((EditText) solo.getView(R.id.habit_input), "Push-Ups");
         solo.enterText((EditText) solo.getView(R.id.reason_input), "I want big arms");
-        solo.clickOnView(solo.getView(R.id.date_input_box));
+        //solo.clickOnView(solo.getView(R.id.date_input));
         // open up picker and select some date
 
-        solo.clickOnView(solo.getView(R.id.freq_input_box));
+        //solo.clickOnView(solo.getView(R.id.freq_input));
         // open up picker and select some days
-
-        solo.clickOnView(solo.getView(R.id.four_star));
         solo.clickOnView(solo.getView(R.id.confirmH_button));
 
         solo.waitForActivity("HabitTabActivity");
