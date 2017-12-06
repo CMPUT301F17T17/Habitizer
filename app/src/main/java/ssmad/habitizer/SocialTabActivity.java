96e9eb6a4f4639bfd2f5b0520acd020af5ad32ba
lmf02013633 <daocaorenatm@hotmail.com>
2017-12-07 07:32:37 +0800

    2

--- app/src/main/java/ssmad/habitizer/SocialTabActivity.java
+++ app/src/main/java/ssmad/habitizer/SocialTabActivity.java
@@ -3,6 +3,8 @@ package ssmad.habitizer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SocialTabActivity extends AppCompatActivity {
 
@@ -13,9 +15,24 @@ public class SocialTabActivity extends AppCompatActivity {
         Intent intent = getIntent();
         intent.getStringExtra("username");
         DummyMainActivity.initTabs(DummyMainActivity.VIEW_SOCIAL, this, intent);
+
+        Button button = (Button) findViewById(R.id.followers_button);
+        Button button2 = (Button) findViewById(R.id.following_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(SocialTabActivity.this, FollowActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(SocialTabActivity.this, FollowActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        if (requestCode == );
     }
 }
